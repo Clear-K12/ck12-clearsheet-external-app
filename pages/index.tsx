d1f14af2d639699580c5d435b154fe0308df6d90
zaid-quadr1 <zaid.quadri@mindruby.com>
2024-04-10 17:32:49 +0530

    Made changes related to contact page

--- pages/index.tsx
+++ pages/index.tsx
@@ -11,7 +11,7 @@ const inter = Inter({ subsets: ['latin'] })
 export default function Home() {
     const navigation = useRouter();
     useEffect(() => {
-      navigation.push(COMMONCONSTANT.ROUTEPATH.REQUESTMOREINFO);
+      navigation.push(COMMONCONSTANT.ROUTEPATH.SIGNUP);
     }, [])
     
     return <div>Loading...</div>;
